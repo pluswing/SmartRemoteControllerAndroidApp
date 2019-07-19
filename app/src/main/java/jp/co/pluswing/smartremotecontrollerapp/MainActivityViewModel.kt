992b348eada059ed391aa9a230bcb738ecb5f305
toshiyuki <toshiyuki.saito.16@gmail.com>
2019-07-19 11:23:38 +0900

    ListView DataBinding part01

--- app/src/main/java/jp/co/pluswing/smartremotecontrollerapp/MainActivityViewModel.kt
+++ app/src/main/java/jp/co/pluswing/smartremotecontrollerapp/MainActivityViewModel.kt
@@ -3,10 +3,12 @@ package jp.co.pluswing.smartremotecontrollerapp
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import jp.co.pluswing.smartremotecontrollerapp.model.IrData
 
-class MainActivityViewModel: ViewModel() {
+class MainActivityViewModel : ViewModel() {
 
     val title = MutableLiveData<String>("あいうえお")
+    val items = MutableLiveData<List<IrData>>(listOf())
 
     fun click() {
         Log.d("TEST", title.value)
---
+++ app/src/main/java/jp/co/pluswing/smartremotecontrollerapp/model/IrData.kt
@@ -0,0 +1,6 @@
+package jp.co.pluswing.smartremotecontrollerapp.model
+
+class IrData {
+    var no: Int = 0
+    var name: String = ""
+}
